16929694589b3df568f5ec7ca2e73d3006b3d504
mgbrando <mgbrando29@gmail.com>
2016-12-23 17:37:22 -0500

    Initial commit for Travel Plan V1

---
+++ javascript/app.js
@@ -0,0 +1,48 @@
+function createCORSRequest(method, url) {
+  	var xhr = new XMLHttpRequest();
+  	if ("withCredentials" in xhr) {
+
+    	// Check if the XMLHttpRequest object has a "withCredentials" property.
+    	// "withCredentials" only exists on XMLHTTPRequest2 objects.
+    	xhr.open(method, url, true);
+
+  	} else if (typeof XDomainRequest != "undefined") {
+
+    		// Otherwise, check if XDomainRequest.
+    		// XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    		xhr = new XDomainRequest();
+    		xhr.open(method, url);
+
+  	} else {
+
+    	// Otherwise, CORS is not supported by the browser.
+    	xhr = null;
+
+  	}
+  		
+  	return xhr;
+}
+
+function makeCORSrequest(urlWithParameters){
+	var url=urlWithParameters;
+	var request = createCORSRequest('GET', url);
+	if (request) {
+		request.send();
+  	}
+  	else
+  		throw new Error('CORS not supported');
+  	
+  	//response handlers
+  	request.onload=function(){
+
+  	}
+
+  	request.onerror=function(){
+  		throw new Error('There was an error making the request.');
+  	}
+}
+
+$(document).ready(function(){
+	
+
+});
